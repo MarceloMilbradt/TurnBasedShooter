298b4180440dba62e46bf964ce693ddf99e8754c
MarceloMilbradt <milbradthz@gmail.com>
2022-08-18 22:48:50 -0300

    World Map created

--- Assets/Scripts/Pathfinding/PathfindingGridDebugObject.cs
+++ Assets/Scripts/Pathfinding/PathfindingGridDebugObject.cs
@@ -7,6 +7,7 @@ public class PathfindingGridDebugObject : GridDebugObject
     [SerializeField] private TextMeshPro gCostText;
     [SerializeField] private TextMeshPro hCostText;
     [SerializeField] private TextMeshPro fCostText;
+    [SerializeField] private SpriteRenderer isWalkableSpriteRenderer;
     private PathNode node;
     public override void SetGridObject(object pathNode)
     {
@@ -17,7 +18,9 @@ public class PathfindingGridDebugObject : GridDebugObject
     {
         base.Update();
         gCostText.text = node.GetGCost().ToString();
+        gCostText.text = node.GetGCost().ToString();
         fCostText.text = node.GetFCost().ToString();
         hCostText.text = node.GetHCost().ToString();
+        isWalkableSpriteRenderer.color = node.IsWalkable() ? new Color(0,1,0,0.1f) : new Color(1, 0, 0, 0.1f);
     }
 }
